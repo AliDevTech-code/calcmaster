8997e2a53b7ca22506a35417a9bca56d060781e6
Muhammad ALi <mralijaan690@gmail.com>
2026-01-06 20:01:21 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10426104
+    "zoneId": 10426747
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
