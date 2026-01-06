6432016343232d1ce91d16b48d12be9330e82608
Muhammad ALi <mralijaan690@gmail.com>
2026-01-06 17:52:25 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10426042
+    "domain": "5gvci.com",
+    "zoneId": 10426104
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
